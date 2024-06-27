5b5b497420f182e192324d77d424a3d20106c8ca
Tzahi Bar <tzahib@jfrog.com>
2024-06-28 00:00:22 +0300

    Test 8

--- npm_builder.sh
+++ npm_builder.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-echo "echo npm_builder"
+# Check if JFrog CLI is installed, if it does print the version, if not exit with error
+function check_jfrog_cli() {
+    if ! command -v jfrog &> /dev/null; then
+        echo "JFrog CLI is not installed. Exiting..."
+        exit 1
+    fi
+    jfrog -v
+}
+
+# Check if NPM is installed, if it does print the version, if not exit with error
+function check_npm() {
+    if ! command -v npm &> /dev/null; then
+        echo "NPM is not installed. Exiting..."
+        exit 1
+    fi
+    npm -v
+}
