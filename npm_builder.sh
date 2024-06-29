b93cdf1fa14fe9a3c001a7995d3851f8a2fd3e64
Tzahi Bar <tzahib@jfrog.com>
2024-06-30 00:27:20 +0300

    Test 21

--- npm_builder.sh
+++ npm_builder.sh
@@ -9,6 +9,21 @@ function configure_npm_yml() {
   filePath='.jfrog/projects/npm.yaml'
   # Update the version to 1.0.0
   sed -i 's/^version: .*/version: 1.0.0/' "${filePath}"
+
+
+  awk '
+/^type: npm$/ {
+    print
+    print "resolver:"
+    print "    repo: tzahi_npm_remote_test"
+    print "    serverId: ghpoc"
+    next
+}
+{ print }
+' "$filePath" > temp.yml && mv temp.yml "$filePath"
+
+
+
   cat "${filePath}"
 }
 
