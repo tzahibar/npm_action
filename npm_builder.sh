aa8cb146f5770ce6ff3d745c37c2d80a5818c929
Tzahi Bar <tzahib@jfrog.com>
2024-06-30 16:06:00 +0300

    Test 31

--- npm_builder.sh
+++ npm_builder.sh
@@ -4,23 +4,38 @@ echo "hi"
 
 # Configure NPM.yml file
 function configure_npm_yml() {
-  local tempYamlFile
+  local tempYamlFile version resolver deployer
+
+  version="${1}"
+  resolver="${2}"
 
   tempYamlFile=$(mktemp /tmp/temp_yaml.XXXXXX.yml)
 
   # Create the temporary YAML file with custom parameters
 cat <<EOL > "$tempYamlFile"
-version: 123
+version: ${version}
 type: npm
-resolver:
-    repo: tzahi_npm_remote_test
-    serverId: ghpoc
 EOL
 
   # Output the temporary YAML file path
   echo "Temporary YAML file created at: $tempYamlFile"
   cat "${tempYamlFile}"
+
+  if [[ -n "${resolver}" ]]; then
+  awk '
+/^type: npm$/ {
+    print
+    print "resolver:"
+    print "    repo: tzahi_npm_remote_test"
+    print "    serverId: ghpoc"
+    next
+}
+{ print }
+' "$tempYamlFile" > temp.yml && mv temp.yml "$tempYamlFile"
+  fi
+
+  cat "${tempYamlFile}"
 }
 
 # Configure NPM.yml file
-configure_npm_yml
+configure_npm_yml "${1}" "${2}"
